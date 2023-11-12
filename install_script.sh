8ed22f41840ea0cca4fa65fc3d568d74fb443149
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-12 11:52:58 +0100

    adding install script

--- install_script.sh
+++ install_script.sh
@@ -1,8 +1,8 @@
-#/bin/sh
+#!/bin/sh
 ln -s $HOME/.config/home_config/zsh-config/.zshrc $HOME/.zshrc
 ln -s $HOME/.config/home_config/zsh-config/.zshenv $HOME/.zshenv
 ln -s $HOME/.config/home_config/kitty $HOME/.config/kitty
 ln -s $HOME/.config/home_config/nvim $HOME/.config/nvim
-
+ln -s $HOME/.config/home_config/.oh-my-zsh $HOME/.oh-my-zsh
 
 
