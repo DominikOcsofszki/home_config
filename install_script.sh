12075d40afdf05cc64eaf36629b47c82a56684dd
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-11-12 18:05:57 +0100

    adding uninstall script

--- install_script.sh
+++ install_script.sh
@@ -2,7 +2,6 @@
 #
 #
 
-# export HOME=/Users/$(echo $USER)  
 export XDG_CONFIG_HOME=$HOME/.config
 export ZSH_CONFIG=$XDG_CONFIG_HOME/home_config/zsh-config
 
@@ -12,10 +11,7 @@ ln -s $HOME/.config/home_config/kitty $HOME/.config/kitty
 ln -s $HOME/.config/home_config/nvim $HOME/.config/nvim
 ln -s $HOME/.config/home_config/.oh-my-zsh $HOME/.oh-my-zsh
 
-echo "export HOME=/Users/dominikocsofszki change to your home name. inside .zshenv"
-# vi $HOME/.zshenv
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
-echo 'source $HOME/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CONFIG/zsh_plugins/zsh-autosuggestions
 git clone https://github.com/Aloxaf/fzf-tab $ZSH_CONFIG/zsh_plugins/fzf-tab
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git  $ZSH_CONFIG/zsh_plugins/zsh-syntax-highlighting
---
+++ uninstall_script.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+#
+
+export XDG_CONFIG_HOME=$HOME/.config
+export ZSH_CONFIG=$XDG_CONFIG_HOME/home_config/zsh-config
+
+rm -r $HOME/.zshrc
+rm -r $HOME/.zshenv
+rm -r $HOME/.config/kitty
+rm -r $HOME/.config/nvim
+rm -r $HOME/.oh-my-zsh
+
+
+
